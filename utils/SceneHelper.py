21971afe112706b49cf00f0c849cf0efb7fb8e42
sherholz <sebastian.herholz@gmail.com>
2026-03-04 19:15:54 +0100

    [SceneHelper] cleanup

--- utils/SceneHelper.py
+++ utils/SceneHelper.py
@@ -1,7 +1,6 @@
 import os, sys, importlib
 
 def loadScenes(scenes_cfg):
-    testCases = []
     sys.path.append(os.path.dirname(scenes_cfg))
     scenesCfg = importlib.import_module(os.path.basename(scenes_cfg))
     if scenesCfg.scenes_directory.startswith("$HERE$"):
